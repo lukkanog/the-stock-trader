c05080845b673414523544d23494ee5c0b88539b
lukkanog <lukanog@hotmail.com>
2021-10-13 21:10:53 -0300

    feat: create header with router links

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <NavHeader/>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavHeader from "./components/NavHeader.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavHeader
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/NavHeader.vue
@@ -0,0 +1,74 @@
+<template>
+    <v-app-bar
+        app
+        dark
+    >
+        <h1 class="title mr-10">
+            <span class="font-weight-bold">Stock</span> 
+            <span class="font-weight-light">Trader</span> 
+        </h1>
+
+        <v-list class="d-flex menu-items" flat color="transparent">
+            <v-list-item 
+                active-class="active-menu-item" 
+                class="menu-item"
+                to="/"
+            >
+                Início
+            </v-list-item>
+
+            <v-list-item 
+                active-class="active-menu-item" 
+                class="menu-item"
+                to="/portfolio"
+            >
+                Portfólio
+            </v-list-item>
+
+            <v-list-item 
+                active-class="active-menu-item" 
+                class="menu-item" 
+                to="/stocks"
+            >
+                Ações
+            </v-list-item>
+        </v-list>
+
+        <v-spacer/>
+
+        <v-list class="d-flex menu-items" flat color="transparent">
+            <v-list-item 
+                class="menu-item body-2" 
+            >
+                Finalizar dia
+            </v-list-item>
+
+            <v-list-item 
+                class="menu-item body-2" 
+            >
+                Salvar e carregar
+            </v-list-item>
+
+            <v-list-item 
+                class="menu-item body-2" 
+            >
+                Saldo: R$10.000,00
+            </v-list-item>
+        </v-list>
+        
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: "NavHeader"
+}
+</script>
+
+<style>
+    .active-menu-item,
+    .menu-item:hover{
+        filter: brightness(70%);
+        transition: .2s ease-in-out;
+    }
+</style>
