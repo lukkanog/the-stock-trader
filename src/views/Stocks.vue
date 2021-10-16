13e1564c91fe881ba193dc7df94fb326270f076e
lukkanog <lukanog@hotmail.com>
2021-10-16 00:49:13 -0300

    feat: use stock component in Stocks page

--- src/views/Stocks.vue
+++ src/views/Stocks.vue
@@ -1,10 +1,25 @@
 <template>
-  <h1>stocks</h1>
+  <v-layout row wrap dark>
+    <stock v-for="stock in stocks" :key="stock.id"/>
+  </v-layout>
 </template>
 
 <script>
-export default {
+import Stock from "../components/Stock.vue"
 
+export default {
+  name: "Stocks",
+  components: {
+    Stock
+  },
+  data: () => ({
+    stocks: [
+      {id: 1, name: "Google", price: 130},
+      {id: 2, name: "Apple", price: 150},
+      {id: 3, name: "Petrobras", price: 80},
+      {id: 4, name: "Banco Inter", price: 56}
+    ]
+  })
 }
 </script>
 
