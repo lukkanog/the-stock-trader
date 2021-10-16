ef2e9af6cceb065a4637480d1c69f239e882adcf
lukkanog <lukanog@hotmail.com>
2021-10-16 00:47:18 -0300

    feat: create Stock component

---
+++ src/components/Stock.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+        <v-card>
+            <v-card-title class="headline">
+               <strong>AÇÃO <small>(Preço: Preço)</small></strong>
+            </v-card-title>
+        </v-card>
+        <v-card>
+            <v-container fill-height>
+                <v-text-field type="number" label="Quantidade"/>
+                <v-btn class="green darken-3 white--text ml-5">Comprar</v-btn>
+            </v-container>
+        </v-card>
+    </v-flex>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
