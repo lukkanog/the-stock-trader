b594a29a728da4bc6dacc82a90fb9ad357bcf7f6
lukkanog <lukanog@hotmail.com>
2021-10-16 01:25:12 -0300

    feat: add buy stock function

--- src/components/Stock.vue
+++ src/components/Stock.vue
@@ -1,14 +1,25 @@
 <template>
     <v-flex class="pr-3 pb-3" xs12 md6 lg4>
-        <v-card>
+        <v-card class="grey darken-3 white--text" tile>
             <v-card-title class="headline">
                <strong>{{stock.name}} <small class="font-weight-light">{{stock.price | money}}</small></strong>
             </v-card-title>
         </v-card>
-        <v-card>
+        <v-card tile>
             <v-container fill-height>
-                <v-text-field type="number" label="Quantidade"/>
-                <v-btn class="green darken-3 white--text ml-5">Comprar</v-btn>
+                <v-text-field 
+                    type="number" 
+                    label="Quantidade"
+                    v-model.number="quantity"
+                    color="purple lighten-2"
+                />
+                <v-btn 
+                    class="purple darken-3 white--text ml-5"
+                    @click="buyStock"
+                    :disabled="quantity <= 0"
+                >
+                    Comprar
+                </v-btn>
             </v-container>
         </v-card>
     </v-flex>
@@ -19,13 +30,28 @@ export default {
     props: [
         'stock'
     ],
+    data: () => ({
+        quantity: 0,
+    }),
+    methods: {
+        buyStock() {
+            const order = {
+                stockID: this.stock.id,
+                stockPrice: this.stock.price,
+                quantity: this.quantity
+            }
+
+            console.log(order)
+            this.quantity = 0
+        }
+    },
     filters: {
-	money(val) {
-		const decimalValue = val.toFixed(2)
-		const valueWithComma = decimalValue.toString().replace(".",",")
-		return `R$${valueWithComma}`
-	}
-}
+        money(val) {
+            const decimalValue = val.toFixed(2)
+            const valueWithComma = decimalValue.toString().replace(".",",")
+            return `R$${valueWithComma}`
+        }
+    }
 }
 </script>
 
