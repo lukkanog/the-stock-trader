aa28939fd0a4ca81356835c2c8d89082c2d28a4e
lukkanog <lukanog@hotmail.com>
2021-10-27 23:58:45 -0300

    feat: create card component to homepage links

---
+++ src/components/Card.vue
@@ -0,0 +1,44 @@
+<template>
+  <router-link :to="route" style="text-decoration: none;">
+    <v-card elevation="5" class="rounded" style="height: 100%">
+        <slot name="additionalInfo"></slot>
+        <v-card-title class="text-h6">
+            <v-img 
+                :src="require(`@/assets/${icon}`)"
+                :lazy-src="require(`@/assets/${icon}`)"
+                max-height="24"
+                max-width="24"
+                class="mr-2"
+            />
+            {{ title }}	
+        </v-card-title>
+    </v-card>
+    </router-link>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props: {
+        route: {
+            type: String,
+            required: true,
+            default: "/"
+        },
+        title: {
+            type: String,
+            required: true,
+            default: "Card"
+        },
+        icon: {
+            type: String,
+            required: true,
+            default: "stocks.png"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,22 +4,30 @@
       <span class="font-weight-bold">Stock</span> 
       <span class="font-weight-light">Trader</span> 
     </h2>
-    <v-row justify="space-around">
-      <v-col xl="3" lg="3" sm="6" xs="12">
+    <v-row justify="space-around" align="stretch">
+      <v-col xl="3" lg="3" sm="2" xs="12">
         <wallet />
       </v-col>
-
+      <v-col xl="3" lg="3" sm="2" xs="12">
+        <card 
+          route="/stocks"
+          title="Ações"
+          icon="stocks.png"
+        />
+      </v-col>
     </v-row>
   </v-layout>
 </template>
 
 <script>
-  import Wallet from '../components/Wallet.vue'
+import Wallet from '../components/Wallet.vue'
+import Card from '@/components/Card'
 
   export default {
     name: 'Home',
     components: {
-      Wallet
+      Wallet,
+      Card
     },
   }
 </script>
