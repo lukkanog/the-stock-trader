d11d96cc5fcb24ea23b560682f4b8f28339b436c
lukkanog <lukanog@hotmail.com>
2021-10-27 22:56:12 -0300

    feat: add transitions to router

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <nav-header/>
     <v-main>
       <v-container class="pt-10">
-        <router-view/>
+        <transition name="slide" mode="out-in">
+          <router-view/>
+        </transition>
       </v-container>
     </v-main>
   </v-app>
@@ -22,3 +24,35 @@ export default {
   }
 };
 </script>
+
+<style>
+  @keyframes slide-in {
+    from {
+      transform: translateY(-50vh);
+      opacity: 0;
+    }
+    to {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+      opacity: 1;
+    }
+    to {
+      transform: translateY(-50vh);
+      opacity: 1;
+    }
+  }
+
+  .slide-enter-active {
+    animation: slide-in 0.3s ease;
+  }
+
+  .slide-leave-active {
+    animation: slide-out 0.3s ease;
+  }
+</style>
