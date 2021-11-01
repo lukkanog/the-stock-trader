3d276f41d4c7b0835226048f0e3f4ba3737ef250
lukkanog <lukanog@hotmail.com>
2021-11-01 16:35:06 -0300

    feat: response navigation menu

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,11 +1,11 @@
 <template>
     <v-app-bar app>
-        <v-toolbar flat>
-            <v-toolbar-title class="mr-10">
-                <span class="font-weight-bold">Stock</span> 
-                <span class="font-weight-light">Trader</span> 
-            </v-toolbar-title>
+        <v-app-bar-title class="mx-5">
+            <span class="font-weight-bold">Stock</span> 
+            <span class="font-weight-light">Trader</span> 
+        </v-app-bar-title>
 
+        <v-toolbar v-if="!this.$vuetify.breakpoint.xsOnly" flat>
             <v-toolbar-items class="menu-items">
                 <v-btn depressed to="/"> Início </v-btn>
                 <v-btn depressed to="/portfolio"> Portfólio </v-btn>
@@ -24,8 +24,42 @@
                 </v-btn>
                 <v-btn depressed class="body-2"> Saldo: {{ funds | money }} </v-btn>
             </v-toolbar-items>
-            
         </v-toolbar>
+
+        <template v-else >
+            <v-spacer />
+            <v-menu 
+                
+                dark 
+                transition="slide-y-transition" 
+                :offset-y="true"
+            >
+                <v-spacer></v-spacer>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-app-bar-nav-icon 
+                        v-on="on" 
+                        v-bind="attrs" 
+                    />
+                </template>
+                <v-list>
+                    <v-list-item>
+                        <v-list-item-title>
+                            <v-btn depressed to="/"> Início </v-btn>
+                        </v-list-item-title>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-list-item-title>
+                            <v-btn depressed to="/portfolio"> Portfólio </v-btn>
+                        </v-list-item-title>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-list-item-title>
+                            <v-btn depressed to="/stocks"> Ações </v-btn>
+                        </v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </template>
     </v-app-bar>
 
 </template>
