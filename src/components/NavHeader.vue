873e7150337e35f7ac96b0575754fd5000f05bb8
lukkanog <lukanog@hotmail.com>
2021-11-01 17:45:34 -0300

    style: fix navigation menu breakpoint

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -5,7 +5,7 @@
             <span class="font-weight-light">Trader</span> 
         </v-app-bar-title>
 
-        <v-toolbar v-if="!this.$vuetify.breakpoint.sm" flat>
+        <v-toolbar v-if="!this.$vuetify.breakpoint.sm && !this.$vuetify.breakpoint.xs" flat>
             <v-toolbar-items class="menu-items">
                 <v-btn depressed to="/"> Início </v-btn>
                 <v-btn depressed to="/portfolio"> Portfólio </v-btn>
